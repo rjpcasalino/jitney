752a6a30e4223cee28bc306a6232dd1c01930869
rjpcasalino <rjpcasalino@yutang>
2020-02-17 08:21:57 -0800

    remove needless quotes

--- schema.sql
+++ schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"email" TEXT(255) NOT NULL UNIQUE,
-	"password" TEXT(255) NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	email TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "stories" (
-	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"headline" TEXT(255) NOT NULL,
-	"byline" TEXT(255) NOT NULL,
-	"content" TEXT(255) NOT NULL
+CREATE TABLE IF NOT EXISTS stories (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	headline TEXT NOT NULL,
+	byline TEXT NOT NULL,
+	content TEXT NOT NULL
 );
 
 
-INSERT INTO `users` VALUES (NULL, "admin@mail.com", "pass");
-INSERT INTO `stories` VALUES (NULL, "Ford to City: Drop Dead", "President Ford tells NYC to shove it", "Blah")
+INSERT INTO users VALUES (NULL, "admin@mail.com", "pass");
+INSERT INTO stories VALUES (NULL, "Ford to City: Drop Dead", "President Ford tells NYC to shove it", "Blah")
