369351e7b5b887ba2b88e06f61896b1bee72fba0
rjpcasalino <ryan@boringtranquility.io>
2020-02-17 07:30:01 -0800

    layout and minor work with login and signup

--- index.py
+++ index.py
@@ -1,8 +1,22 @@
-from flask import Flask, request, after_this_request, make_response, render_template, flash, redirect, url_for
+import sqlite3
+from flask import Flask, request, after_this_request, make_response, render_template, flash, redirect, url_for, g, session
 import time
 app = Flask(__name__)
 ## TODO: env var
 app.secret_key = b'somethingBASAazz/1a!!'
+DATABASE = './database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 def handle_account_signup(e):
     res = 'hello %s' %(e)
@@ -10,13 +24,14 @@ def handle_account_signup(e):
 
 def handle_login():
     error = None;
-    if request.method == 'POST':
-        if request.form['email'] == 'admin@mail.com':
-            flash('You were successfully logged in!')
-            return redirect(url_for('index'))
-        else:
-            flash('ERROR', error)
-            return redirect(url_for('index'))
+    print(request.form)
+    if request.form['email'] == 'admin@mail.com':
+        flash('You were successfully logged in!')
+        session['email'] = request.form['email']
+        return redirect(url_for('index'))
+    else:
+        flash('ERROR', error)
+        return redirect(url_for('index'))
 
 @app.before_request
 def detect_user_language():
@@ -32,6 +47,9 @@ def detect_user_language():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    cur = get_db().cursor()
+    print(dir(cur))
+    print(cur.fetchall())
     for i in dir(request):
         if i == 'headers':
             print(request.headers)
@@ -40,4 +58,21 @@ def index():
     if request.method == 'POST':
         return handle_login()
     else:
-        return render_template('index.html', browser=browser, time=time.ctime())
+        if 'email' in session:
+            return render_template('index.html', email=session['email'], time=time.ctime())
+        return render_template('index.html', time=time.ctime())
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return handle_login()
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return 'logged out!'
