f8e526e185824a73364b3ddc9ad0ba9340d94a40
rjpcasalino <rjpcasalino@yutang>
2020-02-19 10:33:13 -0800

    minor update to schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,3 @@
-DROP TABLE users;
-DROP TABLE stories;
-
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	email TEXT NOT NULL UNIQUE,
@@ -11,8 +8,9 @@ CREATE TABLE IF NOT EXISTS stories (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	headline TEXT NOT NULL,
 	byline TEXT NOT NULL,
-	content TEXT NOT NULL
+	content TEXT NOT NULL,
+	artist TEXT NOT NULL,
+	FOREIGN KEY(artist) REFERENCES users(id)
 );
 
-INSERT INTO users VALUES (NULL, "admin@mail.com", "$2b$12$hAgL80c2cnYhzFfNMqd2wuZqLbxK3vnNoK/HlJetCh9.C0NeO4JDq");
 INSERT INTO stories VALUES (NULL, "Ford to City: Drop Dead", "President Ford tells NYC to shove it", "Blah");
