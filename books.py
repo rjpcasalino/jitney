22ebc47452ff0e3c3ec256721e9336cd296da661
rjpcasalino <rjpcasalino@yutang>
2020-02-19 19:01:35 -0800

    missed books

---
+++ books.py
@@ -0,0 +1,16 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, after_this_request, make_response
+)
+import bcrypt
+import flask_login
+
+from jitney.db import get_db, query_db
+from .user import User
+
+bp = Blueprint('books', __name__, url_prefix='/books')
+
+@bp.route('/', methods=['GET', 'POST'])
+def books():
+    return render_template('books.html')
