5ad979a6050c103b130595b4114742d3c01d1130
rjpcasalino <rjpcasalino@yutang>
2020-02-20 08:48:57 -0800

    query db before request; prob bad idea

--- index.py
+++ index.py
@@ -3,26 +3,17 @@ import time
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, after_this_request, make_response
 )
+
 import flask_login
+from . import db
 
 bp = Blueprint('index', __name__, url_prefix='/')
-
 @bp.before_request
-def detect_user_language():
-    language = request.cookies.get('lang')
-    
-    if language is None:
-        language = 'en-US' ## gonna guess
-
-        @after_this_request
-        def remember_lang(response):
-            response.set_cookie('lang', language)
-            return response
+def load_stories():
+    g.stories = db.query_db('SELECT * FROM stories;', one=True)
 
 @bp.route('/', methods=['GET', 'POST'])
 def frontpage():
-    if 'email' in session:
-        return render_template('front.html', email=session['email'], time=time.ctime())
     return render_template('front.html', time=time.ctime())
 
 @bp.route('/submit', methods=['POST'])
