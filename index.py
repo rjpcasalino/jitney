f871ddb0250880a00429736eac5585b442bce448
rjpcasalino <ryan@boringtranquility.io>
2020-03-24 16:42:47 -0700

    adds urllib3 and adds new route called morning that takes a lat and lng query param and requests from darksky

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import functools
+import urllib3
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, after_this_request, make_response, jsonify
+    Blueprint, flash, current_app, g, redirect, render_template, request, session, url_for, after_this_request, make_response, jsonify
 )
 
 import flask_login
@@ -8,11 +9,13 @@ from . import db
 
 bp = Blueprint("index", __name__, url_prefix="/")
 
+http = urllib3.PoolManager()
 
 @bp.before_request
 def load_stories():
     g.lead_stories = db.query_db(
         "SELECT title, headline, byline, preview, publishYear, publishMonth, publishDay FROM stories WHERE lead = 1;")
+    g.darksky_api = "10e290e9e7bcc2b71c38684cc11a171c"
 
 
 @bp.route("/", methods=["GET", "POST"])
@@ -35,6 +38,10 @@ def submit():
 def account():
     return render_template("account.html")
 
-@bp.route("/heart_beat")
-def heart_beat():
-    return jsonify({"key": "value"})
+@bp.route("/morning")
+def morning():
+    if request.args.get('lat') is None or request.args.get('lng') is None:
+        return 'Bad Request', 400
+    url = f"https://api.darksky.net/forecast/{current_app.config['DARKSKY_API']}/{request.args.get('lat')},{request.args.get('lng')}"
+    r = http.request("GET", url)
+    return r.data
